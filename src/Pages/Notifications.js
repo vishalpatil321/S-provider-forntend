e1c3dd1bdec313407284a5a8c9984e023428dbfd
Vishal Dhone <150796650+vishalpatil321@users.noreply.github.com>
2024-08-11 09:28:20 +0530

    Update Notifications.js

--- src/Pages/Notifications.js
+++ src/Pages/Notifications.js
@@ -11,7 +11,6 @@ const Notifications = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const {user} = useSelector((state) => state.user);
-    console.log(user);
 
     const handleMarkRead = async() => {
        try {
